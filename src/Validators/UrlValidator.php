d987963d6405c2c6623fd421dfa86080e791ff3a
liyuze <290315384@qq.com>
2018-06-29 14:35:08 +0800

    调整验证url的正则表达式，支持指定有效的 url schemes

--- src/Validators/UrlValidator.php
+++ src/Validators/UrlValidator.php
@@ -2,16 +2,19 @@
 
 namespace liyuze\validator\Validators;
 
+use liyuze\validator\Exceptions\InvalidConfigException;
 use liyuze\validator\Parameters\Parameter;
 
 class UrlValidator extends Validator
 {
-    const URL_PROTOCOLS = '((https?|s?ftp|irc[6s]?|git|afp|telnet|smb):\/\/)';     //协议
-    const URL_USER_INFO = '([a-z0-9]\w*(\:[\S]+)?\@)';     //用户账号
+    const URL_PROTOCOLS = '({url_schemes}:\/\/)';     //协议
+    const URL_USER_INFO = '([a-z0-9]\w*(:\w+)?@)';     //用户账号
     const URL_DOMAIN = '([a-z0-9]([\w]*[a-z0-9])*\.)?[a-z0-9]\w*\.[a-z]{2,}(\.[a-z]{2,})'; //域名
     const URL_PORT = '(:\d{1,5})'; //端口
     const URL_IP = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';    //ip
     const URL_ADDRESS = '(\/\S*)'; //地址
+    const URL_QUERY = '(([\?&]?[^?&\s]+=[^&\s#]*)+)'; //url参数
+    const URL_FRAGMENT = '(#[\w\-.]*)'; //url fragment 参数
 
     /**
      * @var string 验证器名称
@@ -21,7 +24,7 @@ class UrlValidator extends Validator
     /**
      * @var string 包含名称的url验证规则
      */
-    public $pattern = '/^https?:\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)(?::\d{1,5})?(?:$|[?\/#])/i';
+    public $pattern = '/^{url_schemes}:\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)(?::\d{1,5})?(?:$|[?\/#])/i';
 
     /**
      * @var array 验证的url部分
@@ -30,15 +33,29 @@ class UrlValidator extends Validator
     public $patternPart;
 
     /**
+     * @var array 有效的URI schemes
+     */
+    public $validSchemes = ['http', 'https', 'ftp', 'sftp', 'file', 'git', 'telnet', 'https', 'https', ];
+
+    /**
      * @var string 错误消息
      */
     public $message = '';
 
+    /**
+     * UrlValidator constructor.
+     * @param array $config
+     */
     public function __construct($config = [])
     {
         parent::__construct($config);
+        if ($this->patternPart !== null) {
+            $this->pattern = '/^'.implode('', (array)$this->patternPart).'$/i';
+        }
 
-        $this->message == '' && $this->message = '{param_name}不是有效的URL。';
+        $this->pattern = str_replace('{url_schemes}', '('.implode('|', $this->validSchemes).')', $this->pattern);
+
+        $this->message == '' && $this->message = '{param_name}的值不是有效的URL。';
     }
 
     /**
@@ -49,12 +66,9 @@ class UrlValidator extends Validator
     protected function _validateParam(Parameter $parameter)
     {
         $value = $parameter->getValue();
-        if ($this->patternPart !== null) {
-            if (!preg_match('/^' . implode('', $this->patternPart) . '$/i', $value))
-                $this->addError($parameter, $this->message);
-        } elseif (!preg_match($this->pattern, $value)) {
+
+        if (!preg_match($this->pattern, $value))
             $this->addError($parameter, $this->message);
-        }
 
         return true;
     }
@@ -66,11 +80,8 @@ class UrlValidator extends Validator
      */
     protected function _validateValue($value)
     {
-        if ($this->patternPart !== null && !preg_match('/'.implode('', $this->patternPart).'/', $value))
+        if (!preg_match($this->pattern, $value))
             return $this->message;
-        elseif (!preg_match($this->pattern, $value)) {
-            return $this->message;
-        }
 
         return true;
     }
