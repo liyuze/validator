252bda7bee2a13b827a435e4a42aad26d8f93ba5
liyuze <290315384@qq.com>
2018-07-15 18:10:21 +0800

    新增支持Yii2 Component功能。

---
+++ src/ValidatorComponent.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace liyuze\validator;
+use liyuze\validator\Common\CreatorTrait;
+use yii\base\Component;
+
+/**
+ * Class ValidatorComponent
+ * @package liyuze\Validators
+ */
+class ValidatorComponent extends Component
+{
+    use CreatorTrait;
+
+
+}
